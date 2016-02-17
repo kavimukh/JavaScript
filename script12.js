129ada40368664103e2dc6109ccd8fc9a7015f9b
Hasmukh Patel <hpatel@ancestry.com>
2016-02-17 10:50:58 -0800

    all existing files added

---
+++ script12.js
@@ -0,0 +1,10 @@
+var batwing = {
+	status: "Ready",
+	rescueBatman: function(){
+		console.log("Locating his transponder ...");
+	}
+}
+
+if (batwing.status === "Ready"){
+	batwing.rescueBatman();
+}
